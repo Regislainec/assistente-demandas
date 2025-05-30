2039d1eac2750b9e14f0f38a1d6e5fc65b0912c5
Regislainec <regislaine.carvalho@gmail.com>
2025-05-30 17:43:57 -0300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from datetime import datetime, timedelta
 from database import get_all_demanda
 from email_utils import enviar_email
+from fastapi import FastAPI
+
+app = FastAPI()
 
 def verificar_prazos():
     hoje = datetime.now().date()
@@ -11,3 +14,7 @@ def verificar_prazos():
             assunto = "⚠️ Prazo de demanda se encerrando"
             corpo = f"A demanda '{demanda['titulo']}' está com prazo de entrega para amanhã.\nResponsável: {demanda['responsavel']}"
             enviar_email(demanda["email_responsavel"], assunto, corpo)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
