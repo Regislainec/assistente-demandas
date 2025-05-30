8471d23c4744bd96c31d08aef824ac012573dcd9
Regislainec <regislaine.carvalho@gmail.com>
2025-05-30 17:39:41 -0300

    database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from models import Demanda
+from database_connection import SessionLocal
 from typing import List
 import sqlite3
 
@@ -33,18 +34,9 @@ def create_demand(demanda: Demanda):
     conn.close()
     return {"mensagem": "Demanda criada com sucesso"}
 
-def get_all_demanda() -> List[Demanda]:
-    conn = sqlite3.connect("demandas.db")
-    cursor = conn.cursor()
-    cursor.execute("SELECT titulo, descricao, nome_responsavel, email_responsavel, prioridade, prazo_entrega, status FROM demandas")
-    rows = cursor.fetchall()
-    conn.close()
-    return [Demanda(
-        titulo=row[0],
-        descricao=row[1],
-        nome_responsavel=row[2],
-        email_responsavel=row[3],
-        prioridade=row[4],
-        prazo_entrega=row[5],
-        status=row[6]
-    ) for row in rows]
+def get_all_demanda():
+    db = SessionLocal()
+    try:
+        return db.query(Demanda).all()
+    finally:
+        db.close()
